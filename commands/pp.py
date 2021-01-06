9042a0d0a4c108eadfa0645213882310530fcfb6
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2021-01-06 15:07:52 -0800

    fix !pp command and /np

--- commands/pp.py
+++ commands/pp.py
@@ -1,12 +1,12 @@
-import requests
+import requests, asyncio
 
 async def pp(ctx, args):
     map = args[1]
     final = ""
     r = requests.get(url=f"https://ripple.moe/letsapi/v1/pp?b={map}").json()
     pp = r["pp"]
+    pp.reverse()
     for i in range(4):
-        j = i+1
         final += f" {i+97}%: {pp[i]}pp |"
     final = r["song_name"] + " | " + final + " *" + str(r["stars"]) + " | BPM " + str(r["bpm"]) + " | AR" + str(r["ar"])
     return final
--- helpers/np.py
+++ helpers/np.py
@@ -3,10 +3,9 @@ import requests
 def pp(map):
     final = ""
     r = requests.get(url=f"https://ripple.moe/letsapi/v1/pp?b={map}").json()
-    print(r)
     pp = r["pp"]
+    pp.reverse()
     for i in range(4):
-        j = i+1
         final += f" {i+97}%: {pp[i]}pp |"
     final = r["song_name"] + " | " + final + " *" + str(r["stars"]) + " | BPM " + str(r["bpm"]) + " | AR" + str(r["ar"])
     return final
