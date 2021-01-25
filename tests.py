50ef75d792e391c135c26b3f7820ae4f0bec9977
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2021-01-25 12:07:52 -0800

    fix tests

--- tests.py
+++ tests.py
@@ -4,17 +4,17 @@ from ratelimiter import RateLimiter
         
 class TestGithub(unittest.TestCase):
     def test_github(self):
-        r = asyncio.run(github({}, ["!github"]))
+        r = asyncio.run(github.github({}, ["!github"]))
         self.assertEqual(r, "The github page for this project can be found at https://github.com/spookybear0/spookybot.")
         
 class TestHelp(unittest.TestCase):
     def test_help(self):
-        r = asyncio.run(help({}, ["!help"]))
+        r = asyncio.run(help.help({}, ["!help"]))
         self.assertEqual(r, "Check out the list of commands here: https://github.com/spookybear0/spookybot/wiki/Command-list.")
         
 class TestPing(unittest.TestCase):
     def test_ping(self):
-        r = asyncio.run(ping({}, ["!ping"]))
+        r = asyncio.run(ping.ping({}, ["!ping"]))
         self.assertEqual(r, "Pong!")
 
 # no real testing (yet)
@@ -22,41 +22,41 @@ class TestPing(unittest.TestCase):
 class TestPP(unittest.TestCase):
     def test_if_fail(self):
         try:
-            r = asyncio.run(pp({}, ["!pp", "1236756"]))
+            r = asyncio.run(pp.pp({}, ["!pp", "1236756"]))
         except Exception as e:
             self.fail(e)
 
 class TestRank(unittest.TestCase):
     def test_manual_type_pp(self):
-        r = asyncio.run(rank({}, ["!rank", "100", "pp"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "100", "pp"], True))
         self.assertLess(2023918, r)
         
     def test_manual_type_pp2(self):
-        r = asyncio.run(rank({}, ["!rank", "200pp", "pp"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "200pp", "pp"], True))
         self.assertLess(1503935, r)
         
     def test_manual_type_pp_str(self):
-        r = asyncio.run(rank({}, ["!rank", "1k", "pp"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "1k", "pp"], True))
         self.assertLess(573709, r)
         
     def test_manual_type_pp_str2(self):
-        r = asyncio.run(rank({}, ["!rank", "1kpp", "pp"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "1kpp", "pp"], True))
         self.assertLess(573709, r)
     
     def test_manual_type_rank(self):
-        r = asyncio.run(rank({}, ["!rank", "300k", "rank"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "300k", "rank"], True))
         self.assertLess(1971, r)
         
     def test_manual_type_rank2(self):
-        r = asyncio.run(rank({}, ["!rank", "300000", "rank"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "300000", "rank"], True))
         self.assertLess(1971, r, 1000)
         
     def test_rank_str(self):
-        r = asyncio.run(rank({}, ["!rank", "1k"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "1k"], True))
         self.assertLess(10492, r, 1000)
         
     def test_rank_pp_str(self):
-        r = asyncio.run(rank({}, ["!rank", "1kpp"], True))
+        r = asyncio.run(rank.rank({}, ["!rank", "1kpp"], True))
         self.assertLess(573709, r, 1000)
         
 # recent can't have real tests as it depends on a recent play
@@ -64,7 +64,7 @@ class TestRank(unittest.TestCase):
 class TestRecent(unittest.TestCase):
     def test_if_fail(self):
         try:
-            r = asyncio.run(recent({}, ["!recent", "WhiteCat"]))
+            r = asyncio.run(recent.recent({}, ["!recent", "WhiteCat"]))
         except Exception as e:
             self.fail(e)
 
@@ -73,7 +73,7 @@ class TestRecent(unittest.TestCase):
 class TestTop(unittest.TestCase):
     def test_if_fail(self):
         try:
-            r = asyncio.run(top({}, ["!top", "1", "WhiteCat"]))
+            r = asyncio.run(top.top({}, ["!top", "1", "WhiteCat"]))
         except Exception as e:
             self.fail(e)
             
@@ -82,7 +82,7 @@ class TestTop(unittest.TestCase):
 class TestUser(unittest.TestCase):
     def test_if_fail(self):
         try:
-            r = asyncio.run(user({}, ["!user", "peppy"]))
+            r = asyncio.run(user.user({}, ["!user", "peppy"]))
         except Exception as e:
             self.fail(e)
 
