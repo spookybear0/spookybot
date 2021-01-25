5ae9f451b19cef161fe72f473cb92da70eac68e3
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2021-01-25 15:22:38 -0800

    Update top.py

--- commands/top.py
+++ commands/top.py
@@ -55,6 +55,7 @@ def num_to_mod(number):
     return mod_list
 
 async def top(ctx, args):
+    print(os.getenv("OSUAPIKEY"))
     api = pyosu.OsuApi(os.getenv("OSUAPIKEY"))
     try:
         amount = int(args[1])
