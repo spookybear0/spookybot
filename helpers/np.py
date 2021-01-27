fd3b7c8886c1ac39a6bdab4deadaf115595ff500
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2021-01-26 22:20:16 -0800

    fix np errors

--- helpers/np.py
+++ helpers/np.py
@@ -4,7 +4,8 @@ client = http3.AsyncClient()
 
 async def pp(map, mods=0):
     final = ""
-    r = await client.get(url=f"https://ripple.moe/letsapi/v1/pp?b={map}&m={mods}").json() # ripple api to get pp
+    r = await client.get(url=f"https://ripple.moe/letsapi/v1/pp?b={map}&m={mods}") # ripple api to get pp
+    r = r.json()
     try:
         pp = r["pp"]
     except KeyError: # gamemode not supported
