0e931406a00d96a1aab7328a1bcd427ccfe7d031
SpookyBear0 <50897638+SpookyBear0@users.noreply.github.com>
2021-02-01 22:55:33 -0800

    stupid auto import

--- helpers/bot.py
+++ helpers/bot.py
@@ -1,11 +1,10 @@
-from spook.spookybot.helpers.db import remove_user
 import discord, os, asyncio, pyosu, textwrap, osu_irc, re
 from discord.ext import commands
 from helpers.config import config
 from helpers.parse import parse_args
 from helpers.classify import Classify
 from helpers.command import parse_commands, init_commands
-from helpers.db import ban_user, get_bugs, get_suggestions, get_users, unban_user, connect_db, log_command, set_last_beatmap, add_user
+from helpers.db import ban_user, get_bugs, get_suggestions, get_users, unban_user, connect_db, log_command, set_last_beatmap, add_user, remove_user
 from helpers.np import pp, process_re
 from contextlib import redirect_stdout
 from io import StringIO
