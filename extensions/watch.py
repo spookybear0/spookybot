3ff5a99f8f5eca8bb544c0ffe80d0d105100ed58
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2023-01-15 13:59:08 -0800

    remove failed plays from !watch

--- extensions/watch.py
+++ extensions/watch.py
@@ -31,7 +31,8 @@ class Watch(Extension):
 
                 if recent.beatmap_id != last_play:
                     ctx.username = user
-                    await command_manager.get_command("recent").func(ctx, username, watch=True)
+                    if not recent.rank == "F":
+                        await command_manager.get_command("recent").func(ctx, username, watch=True)
                     self.watched_users[user][watched.index((username, last_play))] = (username, recent.beatmap_id)
 
         await asyncio.sleep(10)
